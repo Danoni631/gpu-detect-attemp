508050cc1b36db85b5492c15067042aafcb52b9e
DaniloSenseiPlus <danunisonsoi@gmail.com>
2026-09-05 15:51:19 -0300

    Remove outl function from ports.c
    Removed unnecessary outl function for NovaOS.

--- ports.c
+++ ports.c
@@ -1,5 +1,31 @@
+/*
+MIT License
+
+Copyright (c) 2026 DaniloSenseiPlus
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include "ports.h"
 
+
+// These functions are unnecessary on NovaOS
 void outl(uint16_t port, uint32_t value)
 {
     asm volatile ("outl %0, %1" : : "a" (value), "Nd" (port));

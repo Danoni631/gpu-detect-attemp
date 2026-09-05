7a26052c3d3d59c83252b4600b525c9ace8d7a65
DaniloSenseiPlus <danunisonsoi@gmail.com>
2026-09-04 22:41:55 -0300

    Add GPU utility functions in gpu.h

---
+++ gpu.h
@@ -0,0 +1,6 @@
+const char* get_gpu_vendor(uint16_t vendor);
+const char* get_gpu_name(uint16_t vendor, uint16_t device_id);
+
+// These functions are optional
+void show_gpu_info();
+void show_gpu_name();
